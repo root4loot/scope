62ac840bfe581db0bed57c82dfd4f273c1d4d689
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-10-20 17:21:55 +0200

    improve test

--- scope_test.go
+++ scope_test.go
@@ -5,33 +5,82 @@ import (
 )
 
 func TestScope(t *testing.T) {
-	tests := []struct {
-		host       string
-		isIncluded bool
-		isExcluded bool
-		isInScope  bool
-	}{
-		{"192.168.0.1", true, false, true},
-		{"192.168.0.6", false, false, false},
-		{"foo.example.com", true, false, true},
-		{"wildcard.example.com", true, false, true},
-		{"excluded.example.com", false, true, false},
-		{"sub.excluded.com", false, true, false}, // New test for subdomain exclusion
-		{"test.example2.com", true, false, true},
-		{"example.com:8080", true, false, true},
-		{"abc.example.xyz.test", true, false, true},
-		{"notinscope.com", false, false, false},
+	s := NewScope()
+	s.AddInclude("192.168.0.1-5", "192.168.10.0/24", "*.example.com", "example2.com:8080", "*.example.*.test")
+	s.AddExclude("somedomain.com", "exclude.example.com", "192.168.0.6")
+
+	// Test single IPs
+	if !s.IsIncluded("192.168.0.2") {
+		t.Errorf("Expected true for IsIncluded, got false")
 	}
 
-	s := NewScope()
-	s.AddInclude("192.168.0.1-5", "*.example.com", "example.com:8080", "test.example2.com", "*.example.*.test")
-	s.AddExclude("excluded.example.com")
-	s.AddExclude("excluded.com")
+	if s.IsIncluded("192.168.0.7") {
+		t.Errorf("Expected false for IsIncluded, got true")
+	}
 
-	for _, test := range tests {
-		inScope := s.InScope(test.host)
-		if inScope != test.isInScope {
-			t.Errorf("InScope(%s) = %t, expected %t", test.host, inScope, test.isInScope)
-		}
+	if !s.IsExcluded("192.168.0.6") {
+		t.Errorf("Expected true for IsExcluded, got false")
+	}
+
+	if s.IsExcluded("192.168.0.1") {
+		t.Errorf("Expected false for IsExcluded, got true")
+	}
+
+	// Test IP range
+	if !s.IsIncluded("192.168.0.4") {
+		t.Errorf("Expected true for IsIncluded, got false")
+	}
+
+	if s.IsIncluded("192.168.0.8") {
+		t.Errorf("Expected false for IsIncluded, got true")
+	}
+
+	// Test CIDR notation
+	if !s.IsIncluded("192.168.10.50") {
+		t.Errorf("Expected true for IsIncluded, got false")
+	}
+
+	if s.IsIncluded("192.168.11.50") {
+		t.Errorf("Expected false for IsIncluded, got true")
+	}
+
+	// Test Domains
+	if !s.IsIncluded("foo.example.com") {
+		t.Errorf("Expected true for IsIncluded, got false")
+	}
+
+	if s.IsIncluded("bar.otherdomain.com") {
+		t.Errorf("Expected false for IsIncluded, got true")
+	}
+
+	if !s.IsExcluded("exclude.example.com") {
+		t.Errorf("Expected true for IsExcluded, got false")
+	}
+
+	// Test Subdomains
+	if !s.IsExcluded("sub.somedomain.com") {
+		t.Errorf("Expected true for IsExcluded, got false")
+	}
+
+	if s.IsExcluded("sub.otherdomain.com") {
+		t.Errorf("Expected false for IsExcluded, got true")
+	}
+
+	// Test port numbers
+	if !s.IsIncluded("example2.com:8080") {
+		t.Errorf("Expected true for IsIncluded, got false")
+	}
+
+	if s.IsIncluded("example2.com:1234") {
+		t.Errorf("Expected false for IsIncluded, got true")
+	}
+
+	// Test wildcard
+	if !s.IsIncluded("foo.example.bar.test") {
+		t.Errorf("Expected true for IsIncluded, got false")
+	}
+
+	if s.IsIncluded("foo.bar.baz.test") {
+		t.Errorf("Expected false for IsIncluded, got true")
 	}
 }
